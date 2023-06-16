ce7214b143693a6a5b9bdfdf387c974242a67185
Yonathan Bornfeld <yonibor@cs.huji.ac.il>
2023-06-16 11:24:48 +0300

    with return value

--- EX4Resources/VirtualMemory.cpp
+++ EX4Resources/VirtualMemory.cpp
@@ -71,7 +71,7 @@ void getNextFrameRecursive(uint64_t frameIdx, uint64_t originFrame, unsigned int
 
 
 
-void getNextFrame(uint64_t originFrame, int originDepth, uint64_t targetPage, word_t *nextFrame) {
+int getNextFrame(uint64_t originFrame, int originDepth, uint64_t targetPage, word_t *nextFrame) {
     uint64_t maxFrameIdx = 0, farthestPage = targetPage, adressUnlink = 0;
     bool foundEmptyTable = false;
     getNextFrameRecursive(0, originFrame, 0, &maxFrameIdx, &farthestPage, &adressUnlink, 0, 0, 
@@ -83,11 +83,13 @@ void getNextFrame(uint64_t originFrame, int originDepth, uint64_t targetPage, wo
     else if (maxFrameIdx < NUM_FRAMES - 1) {
         *nextFrame = maxFrameIdx + 1;
     }
-    else {
-        assert(farthestPage != targetPage);
+    else if (farthestPage != targetPage) {
         PMread(adressUnlink, nextFrame);
         PMevict(*nextFrame, farthestPage);
         PMwrite(adressUnlink, 0);
+    }
+    else {
+        return 0;
     }
 
     if (originDepth == TABLES_DEPTH - 1) {
@@ -97,10 +99,14 @@ void getNextFrame(uint64_t originFrame, int originDepth, uint64_t targetPage, wo
         setTableZero(*nextFrame);
     }
     assert((*nextFrame) != 0);
+    return 1;
 }
 
 
-uint64_t getPhysicalAddress(uint64_t virtualAddress) {
+int getPhysicalAddress(uint64_t virtualAddress, uint64_t *physicalAddress) {
+    if (virtualAddress >= VIRTUAL_MEMORY_SIZE) {
+        return 0;
+    }
     unsigned int curDepth = 0;
     uint64_t curAddress = 0;
     word_t nextFrame;
@@ -109,14 +115,17 @@ uint64_t getPhysicalAddress(uint64_t virtualAddress) {
         uint64_t curOffset = getCurOffset(virtualAddress, curDepth);
         PMread(curAddress + curOffset, &nextFrame);
         if (nextFrame == 0) {
-            getNextFrame(curAddress / PAGE_SIZE, curDepth, pageIdx, &nextFrame);
+            if (!getNextFrame(curAddress / PAGE_SIZE, curDepth, pageIdx, &nextFrame)) {
+                return 0;
+            }
             PMwrite(curAddress + curOffset, nextFrame);
         }
         curAddress = nextFrame * PAGE_SIZE;
         curDepth++;
     }
     uint64_t curOffset = getCurOffset(virtualAddress, curDepth);
-    return curAddress + curOffset;
+    *physicalAddress = curAddress + curOffset;
+    return 1;
 } 
 
 
@@ -126,14 +135,20 @@ void VMinitialize() {
 
 
 int VMread(uint64_t virtualAddress, word_t* value) {
-    uint64_t physhicalAddress = getPhysicalAddress(virtualAddress);
+    uint64_t physhicalAddress;
+    if (!getPhysicalAddress(virtualAddress, &physhicalAddress)) {
+        return 0;
+    }
     PMread(physhicalAddress, value);
     return 1;
 }
 
 
 int VMwrite(uint64_t virtualAddress, word_t value) {
-    uint64_t physhicalAddress = getPhysicalAddress(virtualAddress);
+    uint64_t physhicalAddress;
+    if (!getPhysicalAddress(virtualAddress, &physhicalAddress)) {
+        return 0;
+    }
     PMwrite(physhicalAddress, value);
     return 1;
 }
@@ -151,10 +166,6 @@ int main(int argc, char const *argv[])
     //     std::cout << binaryNumber << std::endl;
     // }
     VMinitialize();
-    VMwrite(13, 3);
-    word_t value1, value2, value3;
-    VMread(13, &value1);
-    VMread(6, &value2);
-    VMread(31, &value3);
+    int ret = VMwrite(VIRTUAL_MEMORY_SIZE - 1, 5);
     return 0;
 }
